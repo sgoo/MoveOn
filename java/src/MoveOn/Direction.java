2ad7af7cb3b5b3c46c30eab9e1e1f2d63ca1c9e1
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-09-30 11:15:57 +1300

    scott wizardry

--- java/src/MoveOn/Direction.java
+++ java/src/MoveOn/Direction.java
@@ -1,5 +1,45 @@
 package MoveOn;
 
+import java.util.ArrayList;
+
 public enum Direction {
-	N,S,E,W;
+	N, S, E, W;
+
+	private ArrayList<Car> cars;
+
+	private Direction() {
+		cars = new ArrayList<Car>();
+	}
+
+	public void addCar(Car car) {
+		cars.add(car);
+	}
+
+	public void removeCar(Car car) {
+		cars.remove(car);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.toString());
+		int soFar = 0;
+		for (Car car : cars) {
+			for (; soFar < car.distanceFromIntersection; soFar++) {
+				sb.append(' ');
+			}
+			soFar += Car.CAR_LENGTH;
+			String id = car.getCarId() + "";
+			sb.append(id);
+			for (int i = id.length(); i < Car.CAR_LENGTH; i++) {
+				sb.append('-');
+			}
+		}
+		for (; soFar < Intersection.VTL_SPAN; soFar++) {
+			sb.append(' ');
+		}
+		
+		sb.append('|');
+		return sb.toString();
+	}
 }
