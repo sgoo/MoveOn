d865f3c68d703b392f34d69c6627cf2fce88a1a1
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-09-30 10:47:36 +1300

    added fields and implementing tick method

--- java/src/MoveOn/Car.java
+++ java/src/MoveOn/Car.java
@@ -1,14 +1,35 @@
 package MoveOn;
 
-public class Car implements Tickable{
-	
-	
-	public Car(){
-		
+public class Car implements Tickable {
+
+	/**
+	 * Car length Unit: m
+	 */
+	public static final int CAR_LENGTH = 5;
+
+	/**
+	 * Car speed Unit: m/s
+	 */
+	public static final int SPEED = 1;
+
+	public int distanceFromIntersection;
+	public Direction direction;
+
+	public Car(int dist, Direction direction) {
+		this.distanceFromIntersection = dist;
+		this.direction = direction;
 	}
-	
+
 	@Override
 	public void tick(int ticks) {
-		
-	}	
+		// TODO: Check if there is a car in front
+		if (distanceFromIntersection > 0) {
+			distanceFromIntersection -= SPEED;
+			if (distanceFromIntersection < 0){
+				distanceFromIntersection = 0;
+			}
+		} else {
+			// If the light is green go to Xing state
+		}
+	}
 }
