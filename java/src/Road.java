f6a6b80d7bd6ac8bd467003d4df8beb1b96ae652
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-09-29 13:08:03 +1300

    Added toString and peds->bool

--- java/src/Road.java
+++ java/src/Road.java
@@ -3,47 +3,59 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * Represents a stretch of road. Cars can come from two sides.
- * Implemented with two queues.
- * Each side also has a pedestrian crossing.
+ * Represents a stretch of road. Cars can come from two sides. Implemented with
+ * two queues. Each side also has a pedestrian crossing.
  * 
  * @author Roy
- *
+ * 
  */
 public class Road {
-	
-	public enum Side { ONE, TWO	}
-	
+
+	/**
+	 * ONE == NORTH||EAST TWO == SOUTH||WEST
+	 */
+	public enum Side {
+		ONE, TWO
+	}
+
 	private Queue<Car> carsWaiting[] = new Queue[2];
-	private AtomicBoolean pedestrianWaiting[] = new AtomicBoolean[2];
-	
-	public Road() {
+	private AtomicBoolean pedestrianWaiting = new AtomicBoolean(false);
+	private String name;
+
+	public Road(String name) {
 		this.carsWaiting[0] = new ConcurrentLinkedQueue<Car>();
 		this.carsWaiting[1] = new ConcurrentLinkedQueue<Car>();
-	
-		this.pedestrianWaiting[0] = new AtomicBoolean(false);
-		this.pedestrianWaiting[1] = new AtomicBoolean(false);
+		this.name = name;
 	}
-	
+
 	public void addCar(Car car, Side side) {
 		switch (side) {
-			case ONE:
-				this.carsWaiting[0].add(car);
-				break;
-			case TWO:
-				this.carsWaiting[1].add(car);
-				break;
+		case ONE:
+			this.carsWaiting[0].add(car);
+			break;
+		case TWO:
+			this.carsWaiting[1].add(car);
+			break;
 		}
 	}
-	
-	public void addPadestrian(Side side) {
-		switch (side) {
-			case ONE:
-				this.pedestrianWaiting[0].set(true);
-				break;
-			case TWO:
-				this.pedestrianWaiting[1].set(true);
-				break;
+
+	public void addPedestrian() {
+		this.pedestrianWaiting.set(true);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("Road ");
+		sb.append(name);
+		sb.append("\n");
+		for (Queue<Car> queue : carsWaiting) {
+			sb.append("\t|");
+			for (Car car : queue) {
+				sb.append(car.toString());
+				sb.append("|");
+			}
+			sb.append("\n");
 		}
+		return sb.toString();
 	}
 }
