302b43285a64ce76494df98cd553e8205393722f
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-09-30 12:10:51 +1300

    The directions have Lights, added crossing view to the ascii view

--- java/src/MoveOn/Direction.java
+++ java/src/MoveOn/Direction.java
@@ -3,7 +3,15 @@ package MoveOn;
 import java.util.ArrayList;
 
 public enum Direction {
+
 	N, S, E, W;
+
+	static {
+		N.lights = S.lights = new Lights(Lights.Color.G);
+		E.lights = W.lights = new Lights(Lights.Color.G);
+	}
+
+	public Lights lights;
 
 	private ArrayList<Car> cars;
 
@@ -22,9 +30,32 @@ public enum Direction {
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(super.toString());
+
+		// TODO print cars that are crossing
+		int carsPrinted = 0;
+		Car car;
 		int soFar = 0;
-		for (Car car : cars) {
+
+		for (; cars.size() > carsPrinted
+				&& (car = cars.get(carsPrinted)).distanceFromIntersection < 0; carsPrinted++) {
+			String carOut = car.getCarId() + "-"
+					+ (-car.distanceFromIntersection);
+			sb.append(carOut);
+			soFar += Car.CAR_LENGTH;
+			for (int i = carOut.length(); i < Car.CAR_LENGTH; i++) {
+				sb.append('-');
+			}
+		}
+
+		for (; soFar < 5; soFar++) {
+			sb.append(' ');
+		}
+		soFar = 0;
+		sb.append(super.toString());
+		sb.append(lights);
+
+		for (; cars.size() > carsPrinted; carsPrinted++) {
+			car = cars.get(carsPrinted);
 			for (; soFar < car.distanceFromIntersection; soFar++) {
 				sb.append(' ');
 			}
