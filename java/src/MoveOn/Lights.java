b1557dbaad255ed168b39814f0bc4e2a1bcd05c9
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-09-30 12:05:06 +1300

    Colours have G,O,R, can switch colour and have toStr

---
+++ java/src/MoveOn/Lights.java
@@ -0,0 +1,36 @@
+package MoveOn;
+
+public class Lights {
+
+	public Color currentColor;
+
+	public Lights(Color start) {
+		currentColor = start;
+
+	}
+
+	public enum Color {
+		G, O, R
+	}
+
+	public Color switchColor() {
+		switch (currentColor) {
+		case G:
+			return currentColor = Color.O;
+		case O:
+			return currentColor = Color.R;
+		default: // case R
+			return currentColor = Color.G;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return currentColor.toString();
+	}
+
+	public boolean isGreen() {
+		return currentColor == Color.G;
+	}
+
+}
