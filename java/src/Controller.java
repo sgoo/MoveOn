d5b35295a7135e84dc548b3a77fec711ac7b7408
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-09-29 13:08:38 +1300

    Made producer for cars

--- java/src/Controller.java
+++ java/src/Controller.java
@@ -1,4 +1,7 @@
+
 public class Controller {
+	
+	private static final int tickDuration = 1000;
 	
 	/**
 	 * Car Probabilities: 
@@ -6,26 +9,44 @@ public class Controller {
 	 * - index 1 = probability of 1 car per tick, etc ... 
 	 * - Should add up to 1
 	 */
-	public static final float probabilityOfCarsPerTick[] = {0.4f, 0.3f, 0.2f, 0.1f}; 
+	public static final double probabilityOfCarsPerTick[] = {0.4, 0.3, 0.2, 0.1}; 
 	
 	/**
 	 * Pedestrians Probabilities
 	 */
-	public static final float probabilityOfPedestriansPerTick[] = {0.95f, 0.05f};
+	public static final double probabilityOfPedestriansPerTick[] = {0.95, 0.05};
+	
+	/**
+	 * Road Probabilities
+	 */
+	public static final double probabilityOfRoadPerTick[] = {0.5, 0.5};
+	
+	/**
+	 * Road Probabilities
+	 */
+	public static final double probabilityCarTypePerTick[] = {1.0, 0};
+	
+	/**
+	 * Road Probabilities
+	 */
+	public static final double probabilityOfSidePerTick[] = {0.5, 0.5};
+
+	private Road roadNS;
+
+	private Road roadEW;
 	
 	public Controller () throws InterruptedException {
-		
 		// north-south road
-		Road roadNS = new Road();
+		roadNS = new Road("NS");
 		
 		// east-west road
-		Road roadEW = new Road();
-		
+		roadEW = new Road("EW");
 		
 		for(int tick = 0; ; tick++) {
 			System.out.println("Tick " + tick + ":");
 			this.tick();
-			Thread.sleep(200);
+			// tick ~~ 1s IRL
+			Thread.sleep(tickDuration);
 		}
 	}
 	
@@ -33,15 +54,45 @@ public class Controller {
 	 * Time is broken discretely
 	 */
 	public void tick() {
+		produce();
+//		pickLeaderIfNotExist();
+//		changeLightsAsAppropriate();
+//		verify();
 		
 	}
 	
 	/**
-	 * Produce cars and padestrians on the road
+	 * Produce cars and pedestrians on the road
 	 * 
 	 * @param road
 	 */
-	public void produce(Road road) {
+	public void produce() {
+		// choose a whole lot of stuff
+		// Road.
+		Road selectedRoad = roadNS;
 		
+		if(Math.random()>probabilityOfRoadPerTick[0]){
+			selectedRoad = roadEW;
+		}
+		
+		// Cars
+		double carProbability = Math.random();
+		for(int i = 0;i<probabilityOfCarsPerTick.length;i++){
+			carProbability-=probabilityOfCarsPerTick[i];
+			if(carProbability<0){
+				
+				for (int j = 0; j < i; j++) {
+					Road.Side choosenSide = Road.Side.ONE;
+					if (Math.random() >= probabilityOfSidePerTick[0]) {
+						choosenSide = Road.Side.TWO;
+					}
+					selectedRoad.addCar(new VTLCar(), choosenSide);
+				}
+				break;
+			}
+		}
+		// TODO: Peds
+		System.out.println(roadNS.toString());
+		System.out.println(roadEW.toString());
 	}
 }
