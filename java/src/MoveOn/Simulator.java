f3cca911db1f2e306e61fa65314a82bf4c0dab73
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-09-30 10:54:16 +1300

    Added initialize, some random cars and a constructor

--- java/src/MoveOn/Simulator.java
+++ java/src/MoveOn/Simulator.java
@@ -4,22 +4,42 @@ import java.util.ArrayList;
 
 public class Simulator {
 
-	private ArrayList<Tickable> tickables;
+	private ArrayList<Car> cars;
 
-	public void simulate(){
-		tickables = new ArrayList<Tickable>();
+	public Simulator() {
+		cars = new ArrayList<Car>();
+	}
+
+	public void initialize() {
+		cars.add(new Car(15, Direction.N));
+		cars.add(new Car(30, Direction.S));
+		cars.add(new Car(60, Direction.N));
+		cars.add(new Car(30, Direction.E));
+		cars.add(new Car(40, Direction.W));
+		cars.add(new Car(10, Direction.N));
+	}
+
+	public void simulate() {
 		for (int i = 0;; i++) {
-			for(Tickable tickable : tickables){
-				tickable.tick(i);
+			for (Car car : cars) {
+				car.tick(i);
+				System.out.println(car.toString());
+				
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		Simulator simulator = new Simulator();
+		simulator.initialize();
 		simulator.simulate();
 	}
 }
