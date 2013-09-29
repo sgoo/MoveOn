a990e7a290822ddc572d07714cdf7a1d289131e6
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-09-30 10:30:42 +1300

    deleted everything

--- java/src/CarNormal.java
+++
@@ -1,8 +0,0 @@
-public class CarNormal extends Car {
-
-	public CarNormal(String name, String direction) {
-		super(name, direction);
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- java/src/CarVTL.java
+++
@@ -1,9 +0,0 @@
-
-public class CarVTL extends Car{
-
-	public CarVTL(String name, String direction) {
-		super(name, direction);
-		// TODO Auto-generated constructor stub
-	}
-
-}
