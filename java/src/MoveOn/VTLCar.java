9914064e3920a58f9cdebfcce2eaed77daa9f53d
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-09-30 19:11:21 +1300

    Has leader and distance and related methods

--- java/src/MoveOn/VTLCar.java
+++ java/src/MoveOn/VTLCar.java
@@ -1,7 +1,31 @@
 package MoveOn;
 
 public class VTLCar extends Car {
+
+	private boolean leader;
+
 	public VTLCar(int dist, Direction direction) {
 		super(dist, direction);
+	}
+
+	public int getDistance() {
+		return distanceFromIntersection;
+	}
+
+	public boolean isLeader() {
+		return leader;
+	}
+
+	public void setLeader(boolean leader) {
+		this.leader = leader;
+	}
+
+	@Override
+	public String toString() {
+		String car = super.toString();
+		if (leader) {
+			return car.substring(0, 4) + "L";
+		}
+		return car;
 	}
 }
