50c81e46708f1d876a473c64512637dee6638486
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-10-02 13:26:01 +1300

    comments4marks

--- java/src/moveon/cars/Car.java
+++ java/src/moveon/cars/Car.java
@@ -4,6 +4,11 @@ import moveon.simulation.Direction;
 import moveon.simulation.Intersection;
 import moveon.simulation.Tickable;
 
+/**
+ * Car represents a normal car (aka non VTL)
+ * @author Jourdan Harvey, Scott Goodhew, Mike Little, Roy Lin
+ *
+ */
 public class Car implements Tickable {
 
 	/**
@@ -16,6 +21,10 @@ public class Car implements Tickable {
 	 */
 	public static final int SPEED = 1;
 
+	/**
+	 * Count of the number of cars that have been through the system. This forms
+	 * the car id
+	 */
 	private static int CAR_COUNT = 0;
 
 	private int carId;
@@ -23,6 +32,12 @@ public class Car implements Tickable {
 	public int distanceFromIntersection;
 	protected Direction direction;
 
+	/**
+	 * Constructor Increment the car count and set this to this cars id
+	 * 
+	 * @param dist
+	 * @param direction
+	 */
 	public Car(int dist, Direction direction) {
 		this.distanceFromIntersection = dist;
 		this.direction = direction;
@@ -55,22 +70,35 @@ public class Car implements Tickable {
 		return true;
 	}
 
+	/**
+	 * What happens on each tick
+	 * 
+	 * @return false if the car should be removed from the system
+	 */
 	@Override
 	public boolean tick(int ticks) {
-		// TODO: Check if there is a car in front
 		if (distanceFromIntersection > 0 && direction.canAdvance(this)) {
+			// start moving toward the intersection if we can
 			distanceFromIntersection -= SPEED;
 			if (distanceFromIntersection < 0) {
 				distanceFromIntersection = 0;
 			}
 		} else if (distanceFromIntersection == 0 && direction.lights.isGreen()) {
+			// we have moved into the intersection
 			distanceFromIntersection -= SPEED;
-			// If the light is green go to Xing state
+			// If the light is green go to crossing state
 		} else if (distanceFromIntersection < 0) {
-			if (distanceFromIntersection < -(CAR_LENGTH + Intersection.INTERSECTION_SPAN - 2)) {
+
+			if (distanceFromIntersection < -(CAR_LENGTH
+					+ Intersection.INTERSECTION_SPAN - 2)) {
+				// We have finished moving through the intersection so remove
+				// the car from the direction and return false so that the car
+				// is removed from the system
 				direction.removeCar(this);
+
 				return false;
 			} else {
+				// keeping moving through the intersection
 				distanceFromIntersection -= SPEED;
 			}
 		}
@@ -80,8 +108,7 @@ public class Car implements Tickable {
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		String carOut = distanceFromIntersection + "";
-		// String carOut = getCarId() + "-" + distanceFromIntersection;
+		String carOut = String.valueOf(distanceFromIntersection);
 		sb.append(carOut);
 		for (int i = carOut.length(); i < CAR_LENGTH; i++) {
 			sb.append('-');
--- java/src/moveon/cars/VTLCar.java
+++ java/src/moveon/cars/VTLCar.java
@@ -2,14 +2,23 @@ package moveon.cars;
 
 import moveon.simulation.Direction;
 
+/**
+ * VTLCar
+ * @author Jourdan Harvey, Scott Goodhew, Mike Little, Roy Lin
+ *
+ */
 public class VTLCar extends Car {
 
 	private boolean leader;
-
+	
 	public VTLCar(int dist, Direction direction) {
 		super(dist, direction);
 	}
-
+	
+	/**
+	 * VTL Cars are aware of their distance from the intersection
+	 * @return
+	 */
 	public int getDistance() {
 		return distanceFromIntersection;
 	}
