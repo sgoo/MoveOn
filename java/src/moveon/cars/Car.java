f55ed4972b5757e7af65d67c67f9d8eba49d710b
Mike Little <bio595@gmail.com>
2013-10-20 22:21:16 +1300

    fixed invars in Car and Things

--- java/src/moveon/cars/Car.java
+++ java/src/moveon/cars/Car.java
@@ -11,7 +11,7 @@ import moveon.simulation.Tickable;
  * @author Jourdan Harvey, Scott Goodhew, Mike Little, Roy Lin
  * 
  */
-@Invariant({ "distanceFromIntersection >= 0 || lightColor != 2" })
+@Invariant({ "(distanceFromIntersection >= 0 || lightColor != 2) || leftIntersection == 1" })
 public class Car implements Tickable {
 
 	/**
@@ -38,7 +38,7 @@ public class Car implements Tickable {
 	// for JPF
 	int lightColor;
 
-	private boolean leftIntersection;
+	private int leftIntersection=0;
 
 	/**
 	 * Constructor Increment the car count and set this to this cars id
@@ -91,25 +91,28 @@ public class Car implements Tickable {
 			if (distanceFromIntersection < 0) {
 				distanceFromIntersection = 0;
 			}
-		} else if (distanceFromIntersection == 0 && direction.lights.isGreen() || leftIntersection) {
-			// we have moved into the intersection or we are leaving the intersection
+		} else if (distanceFromIntersection == 0 && direction.lights.isGreen()
+				|| leftIntersection == 1) {
+			// we have moved into the intersection or we are leaving the
+			// intersection
 			distanceFromIntersection -= SPEED;
 			// If the light is green go to crossing state
 		} else if (distanceFromIntersection < 0) {
-			
-			if (distanceFromIntersection < -(CAR_LENGTH + Intersection.INTERSECTION_SPAN - 2)) {
+
+			if (distanceFromIntersection < -(CAR_LENGTH
+					+ Intersection.INTERSECTION_SPAN - 2)) {
 				// We have finished moving through the intersection so remove
 				// the car from the direction and return false so that the car
 				// is removed from the system
 				direction.removeCar(this);
-				leftIntersection = true;
+				leftIntersection = 1;
 				return false;
 			} else {
 				// keeping moving through the intersection
 				distanceFromIntersection -= SPEED;
 			}
 		}
-		if(leftIntersection && distanceFromIntersection < -100){
+		if (leftIntersection == 1 && distanceFromIntersection < -100) {
 			return false;
 		}
 
